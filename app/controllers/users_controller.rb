71c24b166e64a21b8905547f478d6b75bbc2183d
DavidRMorphew <morphewd@umich.edu>
2021-02-16 18:04:45 -0500

    user_params method added and validations for user attributes' presence tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    user = User.new(user_params)
+    binding.pry
+  end
+  
   def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
