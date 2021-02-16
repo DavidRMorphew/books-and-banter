dc44d0e6e31b65eb689a9d629775ec4f102d4e20
DavidRMorphew <morphewd@umich.edu>
2021-02-16 18:33:07 -0500

    users#create modified to rerender new page when validations fail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    binding.pry
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       redirect_to user_path(user.id)
     else
       render :new
