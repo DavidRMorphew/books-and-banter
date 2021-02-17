286a808e14eb8bb0ef6edb11a7841f9bd8e83560
DavidRMorphew <morphewd@umich.edu>
2021-02-16 19:07:11 -0500

    sessions_controller created with new view and routes for logging in

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/users/:id', to: 'users#show', as: "user"
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  
 end
