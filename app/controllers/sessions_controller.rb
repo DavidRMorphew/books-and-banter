11ff372a0774f9f770e7605d711f6943acfc0d92
DavidRMorphew <morphewd@umich.edu>
2021-02-16 20:03:30 -0500

    model removed from login form and sessions#create updated to downcase email to find user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,8 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
   end
   def create
     binding.pry
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:email].downcase)
   end
 end
