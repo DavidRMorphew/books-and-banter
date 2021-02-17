c0fd77eaff6a86454e1e54bdc45179094a45fe69
DavidRMorphew <morphewd@umich.edu>
2021-02-16 20:50:50 -0500

    logout session#destroy created and session[:id] corrected to session[:user_id] throughout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,21 @@
 class SessionsController < ApplicationController
   def new
+    # check for logged_in? and redirect if logged in - perhaps redirect_if_logged_in before_action
   end
+  
   def create
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
-      session[:id] = user.id
+      session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
       flash[:message] = "Email and/or password invalid. Please try again."
       render :new
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to '/login'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:id] = @user.id
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
       render :new
