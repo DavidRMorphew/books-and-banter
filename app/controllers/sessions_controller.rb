d6b7a8124a3be7d3e23651a8abffbf3191680638
DavidRMorphew <morphewd@umich.edu>
2021-02-16 20:33:55 -0500

    sessions#create login checks authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,12 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    binding.pry
     user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      session[:id] = user.id
+      redirect_to user_path(user.id)
+    else
+      render :new
+    end
   end
 end
