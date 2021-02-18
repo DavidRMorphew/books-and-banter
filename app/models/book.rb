3b1327788e30be845d87d54052b9a6e94817d41f
DavidRMorphew <morphewd@umich.edu>
2021-02-18 09:47:36 -0500

    Book validations for attribute presence and uniqueness of title & authors added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+    validates :authors, :title, :description, :publisher, :publication_date, :categories, presence: true
+    validates :title, uniqueness: { scope: :authors, message: "with the same author is already in our libray." }
 end
