d0e42de7bfca215018256a2820f37b34f0da2b32
DavidRMorphew <morphewd@umich.edu>
2021-02-18 08:35:37 -0500

    Books seeded to database to test controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Book.create({
+    authors: "George Herbert",
+    isbn: "ISBN 10: 0441013597, ISBN 13: 9780441013593", 
+    title: "Dune", 
+    description: "Follows the adventures of Paul Atreides, the son of a betrayed duke given up for dead on a treacherous desert planet and adopted by its fierce, nomadic people, who help him unravel his most unexpected destiny", 
+    publisher: "Penguin", 
+    publication_date: "2005", 
+    categories: "Fiction"
+    })
+Book.create({
+    authors: "Isaac Asimov", 
+    isbn: "ISBN 13: 9780307593962, ISBN 10: 0307593967", 
+    title: "Foundation: Foundation and Empire ; Second Foundation", 
+    description: "The Foundation, established after the Old Empire gives way to barbarism, fights against a mutant strain called the Mule and tries to get rid of the Second Foundation after learning it will inherit a future Empire.", 
+    publisher: "Everyman's Library", 
+    publication_date: "2010", 
+    categories: "Fiction"
+    })
