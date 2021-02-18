fc479f7b0146392ee7620b242f502d3982cf4db0
DavidRMorphew <morphewd@umich.edu>
2021-02-18 12:46:51 -0500

    #display_authors formats for multiple authors

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,17 @@ class Book < ApplicationRecord
     def display_description
         !self.description.match(/[.]\z/) ? self.description << "." : self.description
     end
+
+    def display_authors
+        authors_array = self.authors.split(",")
+        case authors_array.count
+        when 1
+            self.authors
+        when 2
+            authors_array.join(" and ")
+        when 3..100
+            authors_array[-1] = "and " + authors_array[-1]
+            authors_array.join(", ")
+        end
+    end
 end
