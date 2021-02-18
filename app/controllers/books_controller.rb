e1967c1d9afa5c836cbbac90cc6466a15a79baa6
DavidRMorphew <morphewd@umich.edu>
2021-02-18 08:54:09 -0500

    built basic books show page with link to book index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,16 @@
 class BooksController < ApplicationController
+  # Add before_action to find show
+  
   def new
   end
 
   def index
+    # check for login
     @books = Book.all
   end
 
   def show
+    @book = Book.find_by(id: params[:id])
   end
 
   def edit
