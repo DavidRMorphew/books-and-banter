72d82eda1c1b5ec9dd0c15ccaa1b44e1c0c09ace
DavidRMorphew <morphewd@umich.edu>
2021-02-18 12:21:09 -0500

    #display_description adds period if missing from end of description

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
     validates :authors, :title, :description, :publisher, :publication_date, :categories, presence: true
     validates :title, uniqueness: { scope: :authors, message: "with the same author is already in our libray." }
+
+    def display_description
+        !self.description.match(/[.]\z/) ? self.description << "." : self.description
+    end
 end
