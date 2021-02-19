624f9cfd1ac6737348a4f33d34f5126d79dd2d8b
DavidRMorphew <morphewd@umich.edu>
2021-02-18 22:24:13 -0500

    reviews new action built

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,12 @@
 class ReviewsController < ApplicationController
   before_action :redirect_if_not_logged_in
   def new
+    # binding.pry
+    book = Book.find_by(id: params[:book_id])
+    @review = current_user.reviews.build(reviewed_book: book)
   end
+
+  
 
   def index
   end
