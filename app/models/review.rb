b078080157a5c82690df52c146723432e1084c65
DavidRMorphew <morphewd@umich.edu>
2021-02-18 21:14:26 -0500

    presence validations added to reviews and strong_params

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
     belongs_to :reviewer, class_name: "User"
     belongs_to :reviewed_book, class_name: "Book"
+    validates :title, :content, :rating, presence: true
+
+    private
+    def review_params
+        params.require(:review).permit(:title, :content, :rating, :recommend, :kid_friendly)
+    end
 end
