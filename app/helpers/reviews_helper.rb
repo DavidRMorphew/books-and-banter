cfe562194af105ee99140468d26bb503a4c1a9f0
DavidRMorphew <morphewd@umich.edu>
2021-02-19 11:11:53 -0500

    review_title helper added to reviews helpers

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,4 +2,8 @@ module ReviewsHelper
     def display_boolean_yes_no(boolean)
         true ? "Yes" : "No"
     end
+
+    def review_title
+        tag.h1("'#{@book.title},' reviewed by #{current_user.username}")
+    end
 end
