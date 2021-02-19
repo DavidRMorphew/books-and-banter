b38689067539f24ed156ee08423869decb33b7dd
DavidRMorphew <morphewd@umich.edu>
2021-02-19 18:58:21 -0500

    book_index_title_display helper built

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,9 @@
 module BooksHelper
+    def book_index_title_display
+        if number = params[:number]
+            tag.h1("Top #{number} Highest Rated Books")
+        else
+            tag.h1("Our Library")
+        end
+    end
 end
