7ed452c22b4a200dbc89290ef78e2821981c1660
DavidRMorphew <morphewd@umich.edu>
2021-02-20 15:03:23 -0500

    begin build of chainable books index filters in controller with normalized form_tag keys

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,7 +20,7 @@ class BooksController < ApplicationController
 
   def index
     # check for login
-    # binding.pry
+    search_keys = ["most_recently_added", "has_reviews", "ordered_by_aggregate_price"]
     if params[:most_recently_added]
       @books = Book.most_recently_added
     elsif params[:has_reviews]
