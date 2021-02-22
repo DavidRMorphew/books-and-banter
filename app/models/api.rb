57350eee2bc9961db19b759a7ed8fd8594d03fc0
DavidRMorphew <morphewd@umich.edu>
2021-02-22 11:30:47 -0500

    .fetch_books added to api with url

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,2 +1,7 @@
 class Api < ApplicationRecord
+    
+    def self.fetch_books(query)
+        url = "https://www.googleapis.com/books/v1/volumes?q=#{query}&langRestrict=en&key=#{ENV["GBOOKS_API_KEY"]}"
+    end
+
 end
