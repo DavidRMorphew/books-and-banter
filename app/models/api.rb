28229d81ab2498c596508bedac97fefba355898c
DavidRMorphew <morphewd@umich.edu>
2021-02-22 11:41:23 -0500

    api fetch of book hashes stored as an array built and tested

--- app/models/api.rb
+++ app/models/api.rb
@@ -2,6 +2,12 @@ class Api < ApplicationRecord
     
     def self.fetch_books(query)
         url = "https://www.googleapis.com/books/v1/volumes?q=#{query}&langRestrict=en&key=#{ENV["GOOGLE_BOOKS_API_KEY"]}"
+    
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        google_books_hash = JSON.parse(response)
+        books_array = google_books_hash["items"]
+        binding.pry
     end
 
 end
