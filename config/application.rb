9087daf7a7f7a22fac24944fa834fb12f6ac482c
DavidRMorphew <morphewd@umich.edu>
2021-02-22 11:33:21 -0500

    dotenv gem added and required along with net/http require

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,8 @@ require_relative 'boot'
 require 'rails/all'
 
 require 'pry'
+require 'net/http'
+require 'dotenv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
