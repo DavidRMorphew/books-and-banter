490044c510677be905dc57461a3d7a55e2ccd304
DavidRMorphew <morphewd@umich.edu>
2021-02-22 11:38:16 -0500

    .env tested with github

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,7 +1,7 @@
 class Api < ApplicationRecord
     
     def self.fetch_books(query)
-        url = "https://www.googleapis.com/books/v1/volumes?q=#{query}&langRestrict=en&key=#{ENV["GBOOKS_API_KEY"]}"
+        url = "https://www.googleapis.com/books/v1/volumes?q=#{query}&langRestrict=en&key=#{ENV["GOOGLE_BOOKS_API_KEY"]}"
     end
 
 end
