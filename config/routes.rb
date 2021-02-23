10e7adfa17ca192a169609c7379f4b4a612eaf2a
DavidRMorphew <morphewd@umich.edu>
2021-02-23 09:47:57 -0500

    omniauth dynamic route :provider and button_to on layout navbar added

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
 
   resources :books do
     resources :reviews, only: [:index, :new, :create]
