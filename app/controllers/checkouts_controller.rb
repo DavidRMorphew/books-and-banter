d9c693ed97f8724790dd35367892d5d30fd93174
DavidRMorphew <morphewd@umich.edu>
2021-02-23 14:36:45 -0500

    building button_to for checkouts#create

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,6 +1,8 @@
 class CheckoutsController < ApplicationController
   def create
     binding.pry
+    Book.find_by(id: params[:id])
+    # redirect_to book_path()
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,6 @@ Rails.application.routes.draw do
 
   resources :reviews, only: [:show, :edit, :update, :destroy]
 
+  post '/checkout', to: 'checkouts#create'
 
 end
