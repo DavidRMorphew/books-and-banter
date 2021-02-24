04dea935cec35666cacaa827721ceaf6638df8d4
DavidRMorphew <morphewd@umich.edu>
2021-02-24 13:57:13 -0500

    bookshelper included in checkouts_controller, checkouts#create and update fixed

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,11 +1,16 @@
 class CheckoutsController < ApplicationController
+  include BooksHelper
+  # ask about using this
+
   def create
     # binding.pry
     if book = Book.find_by(id: params[:book_id])
-      binding.pry
-      checkout = current_user.checkouts.create(borrowed_book: book)
-      checkout.checkout_date = Time.now
-      checkout.due_date = checkout.checkout_date + 2.week
+      # binding.pry
+      checkout = current_user.checkouts.create({
+        borrowed_book: book,
+        checkout_date: Time.now,
+        due_date: Time.now + 2.week,
+      })
       book.update(currently_checked_out: true)
       binding.pry
       # book.change_checkout_status
@@ -17,5 +22,11 @@ class CheckoutsController < ApplicationController
     binding.pry
     book = Book.find_by(id: params[:book_id])
     checkout = Checkout.find_by(id: params[:id])
+    if current_borrower(book) == current_user
+      checkout.update(due_date: nil)
+      book.update(currently_checked_out: false)
+    else
+      flash[:message] = "You cannot check in that book"
+    end
   end
 end
