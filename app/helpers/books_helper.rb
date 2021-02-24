89ba44edd36caa75f8c8f72dc4d23b17ce49bac4
DavidRMorphew <morphewd@umich.edu>
2021-02-24 16:04:32 -0500

    book_due_date added to return button on users show page

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -39,7 +39,7 @@ module BooksHelper
         if !book.currently_checked_out
             button_to "Check this book out of the library", book_checkouts_path(book.id)
         elsif current_borrower(book) == current_user
-            button_to "Return Book", book_checkout_path(book.id, current_checkout_record(book).id), method: :patch
+            button_to "Return Book (DUE: #{book_due_date(book)})", book_checkout_path(book.id, current_checkout_record(book).id), method: :patch
         else
             tag.h4("This book is currently checked out.")
         end
