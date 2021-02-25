ae836f64a952450217df218834b81b46a6079612
DavidRMorphew <morphewd@umich.edu>
2021-02-25 13:10:44 -0500

    root to home page in static controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "static#home"
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
