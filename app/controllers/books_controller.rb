3cdc29dbce091f04d02e8fec4346fbe87bbca797
DavidRMorphew <morphewd@umich.edu>
2021-02-26 22:00:55 -0500

    flash[:message] cleared in books#index to avoid unwanted persistence

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BooksController < ApplicationController
-  # Add before_action to find book in show, edit, delete called set_book
+  # Add before_action to find book in show, edit, update, delete called set_book
   before_action :redirect_if_not_logged_in
   before_action :redirect_if_not_admin, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_and_set_book, only: [:show, :edit, :update, :destroy]
   include BooksHelper
 
   def new
@@ -34,6 +35,7 @@ class BooksController < ApplicationController
   end
 
   def index
+    flash[:message] = nil
     if !search_keys_selection.empty?
       search_results = search_filter_chaining_method(search_keys_selection)
       if !search_results.empty?
@@ -48,18 +50,12 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find_by(id: params[:id])
   end
 
   def edit
-    # only for admin
-    @book = Book.find_by(id: params[:id])
-    # link from books#show for admin only
   end
 
   def update
-    # only for admin
-    @book = Book.find_by(id: params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book.id)
     else
@@ -68,20 +64,17 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    # admin only, only when book is not currently checked out
-    @book = Book.find_by(id: params[:id])
-    # !@book.currently_checked_out?
     @book.destroy
     redirect_to books_path
   end
-
-  # add class method of highest_rated with action here and route: books/highest_rated - need method in Book to define it with scope
-
-  # add query method for searching through library - author, rating, categories, title (name_like)
 
   private
 
   def book_params
     params.require(:book).permit(:authors, :isbn, :title, :description, :publisher, :publication_date, :categories)
+  end
+
+  def find_and_set_book
+    @book = Book.find_by(id: params[:id])
   end
 end
